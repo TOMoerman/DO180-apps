78e83c1b6f0d11cf9fe62c321302bc451321b832
Student User <student@workstation.lab.example.com>
2022-10-12 08:37:41 -0400

    another change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This page inteltially not left blank\n";
+print "This page intentially not left blank\n";
+print "No, not again\n";
 ?>
